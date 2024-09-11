cd637b6a4ad6597cdb1db60ff837c88619486278
hassan2-aamir <147127458+hassan2-aamir@users.noreply.github.com>
2024-09-11 10:58:56 +0000

    add comments to lab 1

--- lab1/task1.cpp
+++ lab1/task1.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 
 int main(){
+    //declare different datatype variables
     int an_int;
-    int* a; 
     long a_long;
     double a_double;
     float a_float;
     char a_char;
 
-    cout<<"size of int: "<<sizeof(a)<<" bytes"<<endl;
+    //use sizeof to return bytes of different variables
+    cout<<"size of int: "<<sizeof(an_int)<<" bytes"<<endl;
     cout<<"size of long: "<<sizeof(a_long)<<" bytes"<<endl;
     cout<<"size of double: "<<sizeof(a_double)<<" bytes"<<endl;
     cout<<"size of float: "<<sizeof(a_float)<<" bytes"<<endl;
--- lab1/task3.cpp
+++ lab1/task3.cpp
@@ -5,8 +5,11 @@ int main(){
     int list[5]={3,6,9,12,15};
 	int *pArr= list;
 
+    //loop through array
     for (int i =0 ;i<=4;i++){
         cout<<*(pArr+i)<<" ";
     }
+
+    //endl for readabality
     cout<<endl;
 }
